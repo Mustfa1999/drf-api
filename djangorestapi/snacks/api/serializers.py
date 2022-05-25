538513f986b89ef84125ab78b6b0d54cc0c2fcec
Mustfa Ayyed Alhasanat <mustfaaayyed@gmail.com>
2022-05-25 21:27:19 +0300

    last draft

---
+++ djangorestapi/snacks/api/serializers.py
@@ -0,0 +1,10 @@
+from pyexpat import model
+from rest_framework import serializers
+from snacks.models import Snack
+
+
+class SnackSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id','description','title')
+        model = Snack
+
---
+++ djangorestapi/snacks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from snacks.api.viewset import (SnacksListAPIView, SnacksDetailAPIView)
+
+urlpatterns = [
+    path('api/v1/snacks-list', SnacksListAPIView.as_view(), name='snacks_list'),
+    path('api/v1/<int:pk>', SnacksDetailAPIView.as_view(), name='snack_detail'),
+]
